8052d430d355d88c78918b7df24d6b24fa94596a
Syed Sadat Ali <sadat3332@gmail.com>
2024-06-08 15:09:12 +0530

    Creating grid cells using js

--- js/tetris.js
+++ js/tetris.js
@@ -1,3 +1,36 @@
+function renderGrid(){
+    let gridSize = 200;
+    let rowLength = 10;
+    // render main grid
+    let gridElement=document.getElementById('grid');
+    for(let i = 0;i<gridSize;i++){
+        let gridCell = document.createElement('div');
+        
+        gridElement.appendChild(gridCell);
+        }
+    console.log(gridElement.innerHTML);
+    for(let i = 0;i<rowLength;i++){
+        let bottomCell = document.createElement('div');
+        bottomCell.classList.add('freeze');
+        bottomCell.classList.add('bottom');
+        gridElement.appendChild(bottomCell);
+
+    }
+
+    // render bottom grid
+    let nextGridSize = 16;
+    let nextGridElement = document.querySelector('.next-tetro-grid');
+
+    for(let i =0;i<nextGridSize;i++){
+        let nextGridCell = document.createElement('div');
+        nextGridElement.appendChild(nextGridCell);
+
+    }
+
+
+
+}
+renderGrid();
 document.addEventListener('DOMContentLoaded',()=>{
     const width = 10;
     let grid = Array.from(document.querySelectorAll('.grid div'))
@@ -6,7 +39,6 @@ document.addEventListener('DOMContentLoaded',()=>{
     const restartButton = document.querySelectorAll('.restart-btn')
     let gameOverScreen = document.querySelector('.game-over')
     let isGameOver = false;
-    console.log(restartButton)
     const colors = ['blue','red','purple','green','cyan'];
     let nextGrid = Array.from(document.querySelectorAll('.next-tetro-grid div'))
 
